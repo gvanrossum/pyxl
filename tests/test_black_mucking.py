1c7bad093e93e107e0088cd76cadcf26fa1786a0
Michael J. Sullivan <sully@msully.net>
2019-07-01 16:44:50 -0700

    Add a test about black messing things up

---
+++ tests/test_black_mucking.py
@@ -0,0 +1,13 @@
+# coding: pyxl
+import html
+class A:
+    def foo():
+        lol("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", <foo>
+                                                            <bar />
+                                                        </foo>)
+
+        return (
+            <foo>
+                <bar baz="{baz}" spam="{spam}" eggs="{eggs}" />
+            </foo>
+        )
