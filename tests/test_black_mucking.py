e708ae54e4dcb18d5b295e746561f62a151e413b
Michael J. Sullivan <sully@msully.net>
2019-07-02 16:06:27 -0700

    parens grumble.

--- tests/test_black_mucking.py
+++ tests/test_black_mucking.py
@@ -2,28 +2,28 @@
 import html
 class A:
     def foo():
-        lol = <test_thing
+        lol = (<test_thing
                   test_something_whatever={{"initialsomething": initialsomething,
                                             "thisisirritating": lurrrr,
                                             "whatever": whatever,}}
-              />
+              />)
 
-        foo = <test_thing
+        foo = (<test_thing
                   test_something_whatever={["initialsomething",
                                             "thisisirritating",
                                             "whatever",
                                             "more args",
                   ]}
-              />
+              />)
 
-        baz = <test_thing
+        baz = (<test_thing
                   test_something_whatever={foo("initialsomething",
                                                "thisisirritating",
                                                "whatever",
                                                "more args",
                                                "again",
                                                "asdf",)}
-              />
+              />)
 
         lol("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", <foo>
                                                             <bar />
