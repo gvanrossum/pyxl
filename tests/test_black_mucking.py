96965726853042f6c4430e40ea4b4ea1cfb47688
Michael J. Sullivan <sully@msully.net>
2019-07-01 17:18:32 -0700

    more black test

--- tests/test_black_mucking.py
+++ tests/test_black_mucking.py
@@ -2,6 +2,12 @@
 import html
 class A:
     def foo():
+        lol = <test_thing
+                  test_something_whatever={{"initialsomething": initialsomething,
+                                            "thisisirritating": lurrrr,
+                                            "whatever": whatever,}}
+              />
+
         lol("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", <foo>
                                                             <bar />
                                                         </foo>)
