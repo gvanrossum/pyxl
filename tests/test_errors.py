fc8279d030b272fe0995923e4572d09482c75796
Michael J. Sullivan <sully@msully.net>
2019-07-03 10:46:56 -0700

    Fix test_errors

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -10,7 +10,7 @@ error_cases_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 def _expect_failure(file_name):
     path = os.path.join(error_cases_path, file_name)
     try:
-        with open(path) as f:
+        with open(path, 'rb') as f:
             print(pyxl_decode(f.read()))
         assert False, "successfully decoded file %r" % file_name
     except (PyxlParseError, ParseError):
@@ -20,4 +20,4 @@ def test_error_cases():
     cases = os.listdir(error_cases_path)
     for file_name in cases:
         if file_name.endswith(".txt"):
-            yield (_expect_failure, file_name)
+            _expect_failure(file_name)
